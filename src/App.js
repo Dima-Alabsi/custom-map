b9c8ad907f0e8be5fecf0ec2b8319365ba8828d4
Dima Alabsi <dima.alabsi@livaatverse.com>
2024-05-22 09:44:28 +0300

    add geoJson & map

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Map } from "leaflet";
+import MapComponent from "./Map";
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
